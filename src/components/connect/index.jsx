f25453627e5191f8d40b45c589a8b61112fcd69d
KeithV978 <victorolorunda6@gmail.com>
2024-09-01 11:39:43 +0100

    updated connect to launch

--- src/components/connect/index.jsx
+++ src/components/connect/index.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { ConnectButtonWrapper, Title, Wrapper } from "./styles";
 import { Button } from "../button";
 import { WalletWalletConnect } from "@web3icons/react";
-import LinkRounded from "@mui/icons-material/LinkRounded";
+import LinkRounded from "@mui/icons-material/RocketLaunchRounded";
 
 export const ConnectWallet = () => {
   return (
     <Wrapper>
       <Title variant="h4">
-        Connect Your Wallet <WalletWalletConnect width="50px" height="50px" />{" "}
+        Launch Bot <WalletWalletConnect width="50px" height="50px" />{" "}
       </Title>
       <ConnectButtonWrapper>
         <Button bgColor="#ccc" color="#1c1c1c" border="none">
-          Connect <LinkRounded />
+          Launch <LinkRounded />
         </Button>
       </ConnectButtonWrapper>
     </Wrapper>
